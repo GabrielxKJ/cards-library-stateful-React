01c78ea65019323f36f21c0f7171707e6b204c46
User <gabrielkleber006@gmail.com>
2021-09-10 18:45:11 -0300

    requisito 9.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class addMovie extends React.Component {
+class AddMovie extends React.Component {
   constructor() {
     super();
 
@@ -12,18 +12,45 @@ class addMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // Um evento genérico foi definido para abranger todos os inputs e economizar código
+  // é necessário que o nome do input seja igual ao nome do estado para este truque
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input id="title" type="text" data-testid="title-input" />
+          <input
+            name="title"
+            value={ title }
+            id="title"
+            type="text"
+            data-testid="title-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            name="subtitle"
+            value={ subtitle }
+            id="subtitle"
+            type="text"
+            data-testid="subtitle-input"
+            onChange={ this.handleChange }
+          />
         </label>
       </form>
     );
   }
 }
 
-export default addMovie;
+export default AddMovie;
